0c752116128d0aefe280fde8291d0e88cdb9271f
Zane Little <zanetlittle@gmail.com>
2019-01-23 21:15:04 -0500

    chore: Refactor app back to src/app structure
    Close #4

--- src/app/editTheatrePageOld.php
+++ src/app/editTheatrePageOld.php
@@ -3,17 +3,17 @@
 
 <head>
 	<title>OMTS</title>
-	<link rel="icon" href="./resources/images/logo.png">
+	<link rel="icon" href="../resources/images/logo.png">
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="./resources/css/style.css">
+	<link rel="stylesheet" href="../resources/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Faster+One|Roboto+Condensed" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Faster+One|Monoton|Roboto+Condensed" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="./resources/js/func.js"></script>
+	<script src="../resources/js/func.js"></script>
 </head>
 <body class="body">
 	<header class="mainHeader">
@@ -62,7 +62,6 @@ try
 					<td>City<br><input type="text" name="city" class="enterCity" value="'. $row['city'] .'"/></td>
 				</tr>
 				<tr>
-					<td style="display:none;"><input type="text" name="old_theatre_name" class="old_theatre_name" value="'. $theatre_name .'"/></td>
 					<td><button class="submitUpdateTheatre" type="submit" onclick="return updateTheatre()">Submit</button></td>
 				</tr>
 			</table>
@@ -77,15 +76,14 @@ try
 	foreach($conn->query($stmt) as $row)
 	{
 		echo '
-		<form action="updateAud.php" method="post">
+		<form class="audForm">
 			<table class="editAudTable">
 				<tr>
-					<td>Theatre Number<br><input type="text" name="theatre_num" class="enterNum" value="'. $row['theatre_num'] .'" required/></td>
+					<td>Theatre Number<br><input type="text" name="theatre_num" class="enterNum" value="'. $row['theatre_num'] .'"/></td>
 					<td>Capacity<br><input type="text" name="max_seats" class="maxSeats" value="'. $row['max_seats'] .'"/></td>
 					<td>Screen Size<br><input type="text" name="screen_size" class="screenSize" value="'. $row['screen_size'] .'"/></td>
-					<td><br><button class="submitAudChange" type="submit" name="updateAud">Submit</button></td>
-					<td style="display:none;"><input type="text" name="old_entry" class="oldEntry" value="theatre_name=\'' . $theatre_name . '\' and theatre_num=\'' . $row['theatre_num'] . '\'"/></td>
-					<td><br><button class="removeAud" name="removeBtn" type="submit">Remove</button></td>	
+					<td><br><button class="submitAudChange" type="submit" onclick="return false">Submit</button></td>
+					<td style="visibility:hidden;"><input type="text" name="old_entry" class="oldEntry" value="theatre_name=\'' . $theatre_name . '\' and theatre_num=\'' . $row['theatre_num'] . '\'"/></td>
 				</tr>
 			</table>
 		</form>';
@@ -93,75 +91,58 @@ try
 		$movieStmt = 'SELECT * FROM showings as S WHERE S.theatre_name = "'. $theatre_name .'" AND S.theatre_num = "'. $row['theatre_num'] .'" ORDER BY start_time;';
 
 		echo '
-		
-			
+		<table class="movieTable">
+			<form class="movieAudForm">
 				<table class="editMovieTable">
-	';
+					<tr>
+						<th>Movie Title</th>
+						<th>Theatre Number</th>
+						<th>Start Time</th>
+						<th>Seats Available</th>
+					</tr>';
 
 		foreach($conn->query($movieStmt) as $movieRow)
 		{
 			echo '
 			<tr class="movieRow">
-				<td>
-					<form action="updateShowing.php" method="post">
-						<table style="padding-left:5%">
-							<tr>
-								<th>Movie Title</th>
-								<th>Theatre Number</th>
-								<th>Start Time</th>
-								<th>Seats Available</th>
-							</tr>
-							<tr>
-								<td><input type="text" name="movie_title" class="enterMovie" value="'. $movieRow['movie_title'] .'" required/></td>
-								<td><input type="text" name="theatre_num" class="theatreNum" value="'. $movieRow['theatre_num'] .'" required/></td>
-								<td><input type="text" name="start_time" class="startTime" value="'. $movieRow['start_time'] .'" required/></td>
-								<td><input type="text" name="seats_available" class="seatsAvailable" value="'. $movieRow['seats_available'] .'"/></td>
-								<td style="display:none;"><input type="text" name="old_entry" class="oldEntry" value="theatre_name=\'' . $theatre_name . '\' and theatre_num=\'' . $movieRow['theatre_num'] . '\' and movie_title=\'' . $movieRow['movie_title'] . '\'"/></td>
-								<td><button name="submitBtn" class="submitMovieChange" type="submit">Submit</button></td>
-								<td><button class="removeMovie" name="removeBtn" type="submit">Remove</button></td>		
-							</tr>
-						</table>	
-					</form>												
-				</td>
+				<td><input type="text" name="movie_title" class="enterMovie" value="'. $movieRow['movie_title'] .'"/></td>
+				<td><input type="text" name="theatre_num" class="theatreNum" value="'. $movieRow['theatre_num'] .'"/></td>
+				<td><input type="text" name="start_time" class="startTime" value="'. $movieRow['start_time'] .'"/></td>
+				<td><input type="text" name="seats_available" class="seatsAvailable" value="'. $movieRow['seats_available'] .'"/></td>
+				<td style="display:none;"><input type="text" name="old_entry" class="oldEntry" value="theatre_name=\'' . $theatre_name . '\' and theatre_num=\'' . $movieRow['theatre_num'] . '\' and movie_title=\'' . $movieRow['movie_title'] . '\'"/></td>
+				<td><button class="submitMovieChange" type="submit" onclick="return updateMovie(this)">Submit</button></td>
+				<td><button class="removeMovie" type="submit" onclick="return false">Remove</button></td>															
 			</tr>
 			';
 		}
 
+			echo '
+			<tr>
+				<td><input type="text" name="movie_title" class="enterMovie" value="" placeholder="Enter Movie Name..."/></td>
+				<td><input type="text" name="theatre_num" class="theatreNum" value="" placeholder="Enter Theatre Number..."/></td>
+				<td><input type="text" name="start_time" class="startTime" value="" placeholder="Enter Start Time..."/></td>
+				<td><input type="text" name="seats_available" class="seatsAvailable" value="" placeholder="Enter Seats Available..."/></td>
+				<td><button class="submitMovieChange" type="submit" onclick="return false">Add</button></td>														
+			</tr>
+			';
+
 		echo '		
 		</table>
-			
-		';
+			</form>
+		</table>';
 
 	}
 	echo '
-		<p>Add Theatre</p>
-		<form action="updateAud.php" method="post">
+		<form class="audForm">
 			<table class="editAudTable">
 				<tr>
-					<td>Theatre Number<br><input type="text" name="theatre_num" class="enterNum" value="" placeholder="Enter Theatre Number..." required/></td>
+					<td>Theatre Number<br><input type="text" name="theatre_num" class="enterNum" value="" placeholder="Enter Theatre Number..."/></td>
 					<td>Capacity<br><input type="text" name="max_seats" class="maxSeats" value="" placeholder="Enter Theatre Capacity..."/></td>
 					<td>Screen Size<br><input type="text" name="screen_size" class="screenSize" value="" placeholder="Enter Screen Size..."/></td>
-					<td style="display:none;"><input type="text" name="theatre_name" class="theatre_name" value="'. $theatre_name .'"/></td>
-					<td><br><button class="submitAudChange" type="submit" name="addAud">Add</button></td>
+					<td><br><button class="submitAudChange" type="submit" onclick="return false">Add</button></td>
 				</tr>
 			</table>
 		</form>';
-
-	echo '
-		<p>Add Showing</p>
-		<form action="updateShowing.php" method="post">
-			<table>
-				<tr>
-					<td>Movie Title<br><input type="text" name="movie_title" class="enterMovie" value="" placeholder="Enter Movie Name..." required/></td>
-					<td>Theatre Number<br><input type="text" name="theatre_num" class="theatreNum" value="" placeholder="Enter Theatre Number..." required/></td>
-					<td>Start Time (HH:MM:SS)<br><input type="text" name="start_time" class="startTime" value="" placeholder="Enter Start Time..." required/></td>
-					<td>Seats Available<br><input type="text" name="seats_available" class="seatsAvailable" value="" placeholder="Enter Seats Available..."/></td>
-					<td style="display:none;"><input type="text" name="theatre_name" class="theatre_name" value="'. $theatre_name .'"/></td>
-					<td><br><button class="submitMovieChange" name="addMovie" type="submit">Add</button></td>
-				</tr>
-			</table>	
-		</form>	';
-
 	echo '</table>';
 
 }
