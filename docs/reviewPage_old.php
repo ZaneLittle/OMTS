67edcb0df1345ce8df3bbf68f7f3b58024a9494f
Zane Little <zanetlittle@gmail.com>
2019-01-23 20:58:38 -0500

    chore: refactor app to use /docs format
    Close #4

--- docs/reviewPage_old.php
+++ docs/reviewPage_old.php
@@ -3,24 +3,24 @@
 
 <head>
 	<title>OMTS</title>
-	<link rel="icon" href="../resources/images/ourlogo.png">
+	<link rel="icon" href="./resources/images/ourlogo.png">
 
 	<meta charset="utf-8">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="../resources/css/style.css">
+	<link rel="stylesheet" href="./resources/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Faster+One|Roboto+Condensed" rel="stylesheet">
 
 	<link href="https://fonts.googleapis.com/css?family=Faster+One|Monoton|Roboto+Condensed" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="../resources/js/func.js"></script>
+	<script src="./resources/js/func.js"></script>
 </head>
 
 <body class='body'>
 	<header class="mainHeader">
-		<a href="index.html"><img src="../resources/images/ourlogo.png" alt="OMTS Logo" height="160" width="160"><h1 class="title">ONLINE MOVIE<br>TICKET SERVICE</h1></a>
+		<a href="index.html"><img src="./resources/images/ourlogo.png" alt="OMTS Logo" height="160" width="160"><h1 class="title">ONLINE MOVIE<br>TICKET SERVICE</h1></a>
 		<table class="buttons">		
 			<tr><td><a href="accountPage.php" class="accountButton">ACCOUNT</a></td></tr>
 			<tr><td><a href="index.html" class="logoutButton">LOGOUT</a></td></tr>
