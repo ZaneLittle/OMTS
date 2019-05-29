8df3cd58499321ccfc4222677f755f92d4d18b1d
Zane Little <zanetlittle@gmail.com>
2019-05-28 22:34:23 -0400

    fix route in reroute file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- Redirect -->
 <?php
 Header( "HTTP/1.1 301 Moved Permanently" ); 
-Header( "Location: https://dashboard.heroku.com/apps/online-movie-ticketing-service/app" ); 
+Header( "Location: https://online-movie-ticketing-service.herokuapp.com/app/index.php" ); 
 ?>
