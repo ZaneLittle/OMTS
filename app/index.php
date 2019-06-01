d5a17fc4d4e01862505d8af064c7ab7fdd2de9fd
Zane Little <zanetlittle@gmail.com>
2019-06-01 17:09:02 -0400

    feat: Add WIP alert on index page
    close #29:

--- app/index.php
+++ app/index.php
@@ -1,4 +1,6 @@
 <?php
+$message="Disclaimer: this website is is a work in progress and is not fully functional.";
+echo "<script type='text/javascript'>alert('$message');</script>";
 
 if(isset($_COOKIE['omtsName']))
 {
